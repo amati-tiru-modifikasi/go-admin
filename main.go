f46cee5cbc587dbe97b13d0e2af700ac66ef287f
mdestafadilah <desta.08b@gmail.com>
2022-10-08 22:16:58 +0700

    KONEKSI TCP MASIH BELUM BISA

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+	_, err := gorm.Open(mysql.Open("docker:password@tcp(ambassadordb)/ambassador?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 
 	if err != nil {
 		panic("Gagal Terkoneksi")
