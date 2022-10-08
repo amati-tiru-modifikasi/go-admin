a6f0b85d232450771d8fb07a539283625496046a
mdestafadilah <desta.08b@gmail.com>
2022-10-08 16:17:38 +0700

    podman-compose up :)

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+
+	if err != nil {
+		panic("Gagal Terkoneksi")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
